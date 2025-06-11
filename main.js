a653300b4cbcb80a1916ff279e597176c1c4882a
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-06-11 18:27:05 -0300

    feat: Enviando dados do formulário para a lista de pensamentos.

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 import ui from "./services/ui.js";
+import api from "./services/api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos();
+
+    const formularioPensamento = document.getElementById("pensamento-form");
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario);
 });
+
+async function manipularSubmissaoFormulario(evento) {
+    evento.preventDefault();
+    const id = document.getElementById("pensamento-id").value;
+    const conteudo = document.getElementById("pensamento-conteudo").value;
+    const autoria = document.getElementById("pensamento-autoria").value;
+
+    // const pensamento = {
+    //     id,
+    //     conteudo,
+    //     autoria
+    // }
+
+    try {
+        await api.salvarPensamento({conteudo, autoria})
+        ui.renderizarPensamentos();
+    } catch {
+        alert("Erro ao salvar pensamento");
+    }
+}
