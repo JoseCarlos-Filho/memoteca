42f7dabab816dafaa7355c7f2ec9cd960efb6abc
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-06-11 19:55:35 -0300

    feat: metodos assíncronos buscarPensamentoPorId() e alterarPensamento() da API

--- services/api.js
+++ services/api.js
@@ -25,7 +25,35 @@ const api = {
             throw error;
             
         }
-    }
+    },
+
+    async buscarPensamentoPorId(id) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`);
+            return response.json()
+        } catch {
+            alert("Erro na requisição dos dados");
+            throw error;
+            
+        }
+    },
+
+    async alterarPensamento(pensamento) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        } catch {
+            alert("Erro ao alterar pensamento");
+            throw error;
+            
+        }
+    },
 }
 
 export default api;
