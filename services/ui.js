23cfa99a44bd17be77235572c67f0d9d3bd8162c
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-06-21 18:26:55 -0300

    feat: adicionar exibição e preenchimento da data nos pensamentos

--- services/ui.js
+++ services/ui.js
@@ -7,6 +7,8 @@ const ui = {
         document.getElementById("pensamento-id").value = pensamento.id;
         document.getElementById("pensamento-conteudo").value = pensamento.conteudo;
         document.getElementById("pensamento-autoria").value = pensamento.autoria;
+        document.getElementById("pensamento-data").value = pensamento.data.toISOString().split("T")[0];;
+        document.getElementById("form-container").scrollIntoView();
     },
 
     async renderizarPensamentos(pensamentosFiltrados = null) {
@@ -54,6 +56,17 @@ const ui = {
         const pensamentoAutoria = document.createElement("div");
         pensamentoAutoria.textContent = pensamento.autoria;
         pensamentoAutoria.classList.add("pensamento-autoria");
+
+        const pensamentoData = document.createElement("div");
+        const dataFormatada = pensamento.data.toLocaleDateString("pt-BR", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            timeZone: "UTC"
+        });
+        pensamentoData.textContent = dataFormatada;
+        pensamentoData.classList.add("pensamento-data");
 
         const botaoEditar = document.createElement("button");
         botaoEditar.classList.add("botao-editar");
@@ -112,6 +125,8 @@ const ui = {
         li.appendChild(iconeAspas);
         li.appendChild(pensamentoConteudo);
         li.appendChild(pensamentoAutoria);
+        li.appendChild(pensamentoData);
+
         li.appendChild(icones);
         listaPensamentos.appendChild(li);
     },
